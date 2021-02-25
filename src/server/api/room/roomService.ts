7a282a2a4f4d37562cf6f7dcd48a68b7d1b8d942
khai93 <33293519+khai93@users.noreply.github.com>
2021-02-25 00:56:21 -0600

    add RoomSettingsSubscriber, add editRoom method to room

--- src/server/api/room/roomService.ts
+++ src/server/api/room/roomService.ts
@@ -2,12 +2,20 @@ import { IRoomModule, IRoom } from "src/server/modules/room/types";
 import { Logger } from "src/server/modules/logger/logger";
 import { RandomUtility } from "src/server/utils/random";
 import { VideoSourceUtility } from "src/server/utils/videoSource";
+import { RoomSettingChangedPayload } from "./subscribers/settings";
 
 export type RoomServiceDependencies = {
     roomModule: IRoomModule,
     logger: Logger,
     randomUtility: RandomUtility,
     videoSourceUtility: VideoSourceUtility
+}
+
+export enum RoomSetting {
+    Name,
+    MaxUsers,
+    RoomPassword,
+    IsPrivate
 }
 
 export class RoomService {
@@ -26,4 +34,12 @@ export class RoomService {
     }
 
     public getRoom = (roomCode: string): IRoom => this.dependencies.roomModule.getRoom(roomCode)
+
+    public editRoomSetting = (data: RoomSettingChangedPayload) => {
+        const RoomSettingEnum = RoomSetting[data.settingName];
+
+        if (RoomSettingEnum) {
+            this.dependencies.roomModule.editRoom(data.roomCode, data.settingName, data.value);
+        }
+    }
 }
