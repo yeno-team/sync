67d24ae2d4a100d28fd6860c6387ecf2f6f660e4
khai93 <33293519+khai93@users.noreply.github.com>
2021-03-01 02:38:00 -0600

    implement new client structure for socket connection

---
+++ src/client/src/scenes/RoomScreen/EnterRoomPassword.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Modal from 'react-bootstrap'
+
+function EnterRoomPassword(props) {
+    return (
+        <h1> Hello World </h1>
+    )
+}
+
+export default EnterRoomPasswordModal
