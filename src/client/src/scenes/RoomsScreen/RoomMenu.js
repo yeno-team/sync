11d6c91263f3d8af4cf08839ab6d34875225c1fa
Luna <bedgowns@gmail.com>
2021-03-23 19:36:33 -0700

    Filter rooms by room name or code using an input

--- src/client/src/scenes/RoomsScreen/RoomMenu.js
+++ src/client/src/scenes/RoomsScreen/RoomMenu.js
@@ -1,22 +1,58 @@
-import React , { useState } from 'react';
+import React , { useState , useEffect } from 'react';
+import Axios from 'axios';
 import CreateRoomForm from './CreateRoomForm';
 import Rooms from './Rooms';
+import socketSubscriber from '../../api/socket/socketSubscriber';
 import './index.css'
 import "video-react/dist/video-react.css";
 
+const NEW_ROOM_CREATED_EVENT = "NewRoomCreated";
+
 export const RoomMenu = (props) => {
+    const [ search , setSearch ] = useState("")
+    const [ rooms , setRooms ] = useState([])
+    const [ filteredRooms , setFilteredRooms ] = useState([])
     const [show , setModalShow] = useState(false)
     
+    useEffect(() => {
+        (async () => {
+            // Fetch current rooms
+            const req = await Axios({
+                url : "http://localhost:8000/api/room/list",
+                method : "GET"
+            })
+    
+            setRooms(req.data)
+            setFilteredRooms(req.data)
+        })()
+    } , [])
+    
+    useEffect(() => {
+            socketSubscriber.on(NEW_ROOM_CREATED_EVENT , (data) => {
+                setRooms((prevState) => [data , ...prevState])
+            });
+    
+            return () => {
+                socketSubscriber.off(NEW_ROOM_CREATED_EVENT)
+            }
+    } , [])
+    
+
+    useEffect(() => {
+        const filteredRooms = rooms.filter(({ name , code }) => name.startsWith(search) || code.startsWith(search))
+        setFilteredRooms(filteredRooms)
+    } , [search])
+
     return (
         <main>
             <CreateRoomForm show={show} onHide={() => setModalShow(false)} />
             <section class="room-options">
                 <button id="create-room-button" onClick={() => setModalShow(true)}>Create A Room</button>
                 <p id="or">OR</p>
-                <input type="text" name="search-room" id="search-input" placeholder="Search for rooms with text or room code..."/>
+                <input type="text" name="search-room" id="search-input" placeholder="Search for rooms with text or room code..." onChange={(e) => setSearch(e.target.value)} value={search}/>
              </section>
              <section className="rooms">
-                 <Rooms/>
+                 <Rooms rooms={filteredRooms}/>
              </section>
         </main>
     )
--- src/client/src/scenes/RoomsScreen/Rooms.js
+++ src/client/src/scenes/RoomsScreen/Rooms.js
@@ -1,39 +1,11 @@
-import React , { useState , useEffect } from 'react';
-import socketSubscriber from '../../api/socket/socketSubscriber';
+import React from 'react';
 import { useHistory } from 'react-router-dom';
-import Axios from 'axios';
-
-const NEW_ROOM_CREATED_EVENT = "NewRoomCreated";
 
 const Rooms = (props) => {
-    const [ currentRooms , setCurrentRooms ] = useState([])
     const history = useHistory()
 
-    useEffect(() => {
-        (async () => {
-            // Fetch current rooms
-            const req = await Axios({
-                url : "http://localhost:8000/api/room/list",
-                method : "GET"
-            })
-
-            setCurrentRooms(req.data)
-        })()
-    } , [])
-
-    useEffect(() => {
-        socketSubscriber.on(NEW_ROOM_CREATED_EVENT , (data) => {
-            setCurrentRooms((prevState) => [data , ...prevState])
-        });
-
-        return () => {
-            socketSubscriber.off(NEW_ROOM_CREATED_EVENT)
-        }
-    } , [])
-
-
     return (
-        currentRooms.map((room) => (
+        props.rooms && props.rooms.map((room) => (
             <div className="room" data-room-code={room.code} data-room-name={room.name} key={`${room.code}`} onClick={() => history.push(`/room/${room.code}`)}>
                 <div className="room-content">
                     <h3 className="room-name">{room.name}</h3>
