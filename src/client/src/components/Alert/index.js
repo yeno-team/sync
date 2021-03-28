a3e41ec649b64b34edcf77504aae359123ed8634
Luna <bedgowns@gmail.com>
2021-03-27 21:54:39 -0700

    Added/Replaced colors for our color palette

--- src/client/src/components/Alert/index.js
+++ src/client/src/components/Alert/index.js
@@ -1,12 +1,11 @@
 import React , { useState } from 'react';
-import Alert from 'react-bootstrap/Alert';
 
-export default function AlertContainer(props) {
-    const [ show , setShow ] = useState(props.show || true)
-
-    return (
-        <Alert dismissible={props.dismissible || false} variant={props.variant} show={show} onClose={() => setShow(false)}>
-            {props.text}
-        </Alert>
-    )
+export default function Alert({
+    variant,
+    onClick,
+    dismissable,
+    className,
+    children
+}) {
+    
 }
