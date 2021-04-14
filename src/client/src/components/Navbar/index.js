148af154eadc6e11e331ae8c99c635568ba2ae6c
khai93 <33293519+khai93@users.noreply.github.com>
2021-04-14 00:40:02 -0500

    make logo on homepage link to home

--- src/client/src/components/Navbar/index.js
+++ src/client/src/components/Navbar/index.js
@@ -8,7 +8,7 @@ import logo from '../../assets/icons/logo.svg';
 export function Navbar () {
     return (
         <header className="navbar__ctn">
-            <div className="logo"><img src={logo} alt="Sync Logo"/></div>
+            <div className="logo"><Link to="/" ><img src={logo} alt="Sync Logo"/></Link></div>
             <nav className="navigation">
                 <ul>
                     <li><Link to="/" className="navigation-link">Features</Link></li>
