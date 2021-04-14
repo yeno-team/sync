d300b518e1603ebb0dc6c5c36122042d3c682f93
khai93 <33293519+khai93@users.noreply.github.com>
2021-04-13 20:42:06 -0500

    add ttypescript + ts-transformer-imports

--- scripts/build.js
+++ scripts/build.js
@@ -1,7 +1,7 @@
 const exec = require('child_process').execSync;
 
 // Server Build
-exec('npx tsc', {stdio:[0,1,2]});
+exec('npx ttsc', {stdio:[0,1,2]});
 
 // Client Build
 exec("cd ./src/client && yarn build", {stdio:[0,1,2]});
